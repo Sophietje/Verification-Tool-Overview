e7a0375a9d6eb1df33a215a1bd56fe08e827f1e1
Vadim Zaytsev <vadim@grammarware.net>
2022-01-21 12:32:00 +0100

    Adding tests show 20 failing cases for markdown conversion - need to rewrite the replacing hack into a FSM

--- iceberg.py
+++ iceberg.py
@@ -9,61 +9,61 @@ class TestConverter(unittest.TestCase):
 
     def test_matched2code_1_ee(self):
         self.assertEqual(hyper.matched2code('`'), '`')
-    def test_matched2code_1_ea(self):
+    def test_matched2code_1_eA(self):
         self.assertEqual(hyper.matched2code('`a'), '`a')
-    def test_matched2code_1_ae(self):
+    def test_matched2code_1_Ae(self):
         self.assertEqual(hyper.matched2code('a`'), 'a`')
-    def test_matched2code_1_aa(self):
+    def test_matched2code_1_AA(self):
         self.assertEqual(hyper.matched2code('a`b'), 'a`b')
 
     def test_matched2code_2_eee(self):
         self.assertEqual(hyper.matched2code('``'), '')
-    def test_matched2code_2_eea(self):
+    def test_matched2code_2_eeA(self):
         self.assertEqual(hyper.matched2code('``a'), 'a')
-    def test_matched2code_2_eae(self):
+    def test_matched2code_2_eAe(self):
         self.assertEqual(hyper.matched2code('`a`'), '<code>a</code>')
-    def test_matched2code_2_eaa(self):
+    def test_matched2code_2_eAA(self):
         self.assertEqual(hyper.matched2code('`a`b'), '<code>a</code>b')
-    def test_matched2code_2_aee(self):
+    def test_matched2code_2_Aee(self):
         self.assertEqual(hyper.matched2code('a``'), 'a')
-    def test_matched2code_2_aea(self):
+    def test_matched2code_2_AeA(self):
         self.assertEqual(hyper.matched2code('a``b'), 'ab')
-    def test_matched2code_2_aae(self):
+    def test_matched2code_2_AAe(self):
         self.assertEqual(hyper.matched2code('a`b`'), 'a<code>b</code>')
-    def test_matched2code_2_aaa(self):
+    def test_matched2code_2_AAA(self):
         self.assertEqual(hyper.matched2code('a`b`c'), 'a<code>b</code>c')
 
     def test_matched2code_3_eeee(self):
         self.assertEqual(hyper.matched2code('```'), '')
-    def test_matched2code_3_eeea(self):
+    def test_matched2code_3_eeeA(self):
         self.assertEqual(hyper.matched2code('```a'), '<code>a</code>')
-    def test_matched2code_3_eeae(self):
+    def test_matched2code_3_eeAe(self):
         self.assertEqual(hyper.matched2code('``a`'), 'a')
-    def test_matched2code_3_eeaa(self):
+    def test_matched2code_3_eeAA(self):
         self.assertEqual(hyper.matched2code('``a`b'), 'a<code>b</code>')
-    def test_matched2code_3_eaee(self):
+    def test_matched2code_3_eAee(self):
         self.assertEqual(hyper.matched2code('`a``'), '<code>a</code>')
-    def test_matched2code_3_eaea(self):
+    def test_matched2code_3_eAeA(self):
         self.assertEqual(hyper.matched2code('`a``b'), '<code>ab</code>')
-    def test_matched2code_3_eaae(self):
+    def test_matched2code_3_eAAe(self):
         self.assertEqual(hyper.matched2code('`a`b`'), '<code>a</code>b')
-    def test_matched2code_3_eaaa(self):
+    def test_matched2code_3_eAAA(self):
         self.assertEqual(hyper.matched2code('`a`b`c'), '<code>a</code>b<code>c</code>')
-    def test_matched2code_3_aeee(self):
+    def test_matched2code_3_Aeee(self):
         self.assertEqual(hyper.matched2code('a```'), 'a')
-    def test_matched2code_3_aeea(self):
+    def test_matched2code_3_AeeA(self):
         self.assertEqual(hyper.matched2code('a```b'), 'a<code>b</code>')
-    def test_matched2code_3_aeae(self):
+    def test_matched2code_3_AeAe(self):
         self.assertEqual(hyper.matched2code('a``b`'), 'ab')
-    def test_matched2code_3_aeaa(self):
+    def test_matched2code_3_AeAA(self):
         self.assertEqual(hyper.matched2code('a``b`c'), 'ab<code>c</code>')
-    def test_matched2code_3_aaee(self):
+    def test_matched2code_3_AAee(self):
         self.assertEqual(hyper.matched2code('a`b``'), 'a<code>b</code>')
-    def test_matched2code_3_aaea(self):
+    def test_matched2code_3_AAeA(self):
         self.assertEqual(hyper.matched2code('a`b``c'), 'a<code>bc</code>')
-    def test_matched2code_3_aaae(self):
+    def test_matched2code_3_AAAe(self):
         self.assertEqual(hyper.matched2code('a`b`c`'), 'a<code>b</code>c')
-    def test_matched2code_3_aaaa(self):
+    def test_matched2code_3_AAAA(self):
         self.assertEqual(hyper.matched2code('a`b`c`d'), 'a<code>b</code>c<code>d</code>')
 
     def test_internal_link_expl(self):
@@ -86,7 +86,65 @@ class TestConverter(unittest.TestCase):
         self.assertEqual(hyper.link2link(\
             'It is part of [CProver](../Frameworks/CProver.md)'),
             'It is part of <a href="cprover.html">CProver</a>')
-        
+
+    def test_text2text_ee(self):
+        self.assertEqual(hyper.my_md_converter('_'), '_')
+    def test_text2text_eA(self):
+        self.assertEqual(hyper.my_md_converter('_a'), '_a')
+    def test_text2text_Ae(self):
+        self.assertEqual(hyper.my_md_converter('a_'), 'a_')
+    def test_text2text_AA(self):
+        self.assertEqual(hyper.my_md_converter('a_b'), 'a_b')
+
+    def test_text2text_eee(self):
+        self.assertEqual(hyper.my_md_converter('__'), '__')
+    def test_text2text_eeA(self):
+        self.assertEqual(hyper.my_md_converter('__a'), '__a')
+    def test_text2text_eAe(self):
+        self.assertEqual(hyper.my_md_converter('_a_'), '<em>a</em>')
+    def test_text2text_eAA(self):
+        self.assertEqual(hyper.my_md_converter('_a_b'), '<em>a</em>b')
+    def test_text2text_Aee(self):
+        self.assertEqual(hyper.my_md_converter('a__'), 'a__')
+    def test_text2text_AAe(self):
+        self.assertEqual(hyper.my_md_converter('a_b_'), 'a<em>b</em>')
+    def test_text2text_AeA(self):
+        self.assertEqual(hyper.my_md_converter('a__b'), 'a__b')
+    def test_text2text_AAA(self):
+        self.assertEqual(hyper.my_md_converter('a_b_c'), 'a<em>b</em>c')
+
+    def test_text2text_eeee(self):
+        self.assertEqual(hyper.my_md_converter('___'), '___')
+    def test_text2text_eeeA(self):
+        self.assertEqual(hyper.my_md_converter('___a'), '___a')
+    def test_text2text_eeAe(self):
+        self.assertEqual(hyper.my_md_converter('__a_'), '_<em>a</em>')
+    def test_text2text_eeAA(self):
+        self.assertEqual(hyper.my_md_converter('__a_b'), '_<em>a</em>b')
+    def test_text2text_eAee(self):
+        self.assertEqual(hyper.my_md_converter('_a__'), '<em>a</em>_')
+    def test_text2text_eAeA(self):
+        self.assertEqual(hyper.my_md_converter('_a__b'), '<em>a</em>_b')
+    def test_text2text_eAAe(self):
+        self.assertEqual(hyper.my_md_converter('_a_b_'), '<em>a</em>b_')
+    def test_text2text_eAAA(self):
+        self.assertEqual(hyper.my_md_converter('_a_b_c'), '<em>a</em>b_c')
+    def test_text2text_Aeee(self):
+        self.assertEqual(hyper.my_md_converter('a___'), 'a___')
+    def test_text2text_AeeA(self):
+        self.assertEqual(hyper.my_md_converter('a___b'), 'a___b')
+    def test_text2text_AeAe(self):
+        self.assertEqual(hyper.my_md_converter('a__b_'), 'a_<em>b</em>')
+    def test_text2text_AeAA(self):
+        self.assertEqual(hyper.my_md_converter('a__b_c'), 'a_<em>b</em>c')
+    def test_text2text_AAee(self):
+        self.assertEqual(hyper.my_md_converter('a_b__'), 'a<em>b</em>_')
+    def test_text2text_AAeA(self):
+        self.assertEqual(hyper.my_md_converter('a_b__c'), 'a<em>b</em>_c')
+    def test_text2text_AAAe(self):
+        self.assertEqual(hyper.my_md_converter('a_b_c_'), 'a<em>b</em>c_')
+    def test_text2text_AAAA(self):
+        self.assertEqual(hyper.my_md_converter('a_b_c_d'), 'a<em>b</em>c_d')
 
 if __name__ == '__main__':
     unittest.main()
