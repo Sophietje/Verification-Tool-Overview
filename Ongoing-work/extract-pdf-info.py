b9999f24921ae1d6b526aa1decb1fa0c4444fcc4
Sophie Lathouwers <sophie.lathouwers@gmail.com>
2022-09-19 17:23:24 +0200

    Fix title recognition for CAV papers, add method to generate MD files

--- Ongoing-work/extract-pdf-info.py
+++ Ongoing-work/extract-pdf-info.py
@@ -7,38 +7,111 @@
 
 from PyPDF2 import PdfReader
 import os
+import re
+md_template = """
+#### Name:
+
+
+#### Application domain/field:
+
+
+#### Type of tool (e.g. model checker, test generator):
+
+
+#### Expected input thing:
+
+
+#### Expected input format:
+
+
+#### Expected output:
+
+
+#### Internals (tools used, frameworks, techniques, paradigms, ...):
+
+
+#### Comments:
+
+
+#### URIs (github, websites, etc.):
+
+
+#### Last commit date:
+
+
+#### Last publication date:
+
+
+#### List of related papers:
+{}
+
+#### Related tools (tools mentioned or compared to in the paper):
+
+
+#### Meta
+{}
+"""
+
+#TODO: recognize artifact badges
+
+# Generate a markdown file for all papers found with corresponding titles, keywords and links
+def dump_to_markdown(paper_title, doi, conf, year, keywords):
+  name = ''.join(e for e in paper_title if e.isalnum())
+  filename = "generated/"+name+".md"
+  with open(filename, 'w', encoding='utf-8') as file:
+    paper_link = doi + " (" + conf + " "+ year + ")"
+    keywords = "".join([":: "+k.replace("-\n", "")+"\n" for k in keywords])
+    p = md_template.format(paper_link, keywords)
+    file.write(p)
+
 
 
 # Extracts links from a PDF page by looking for "http" in sentences
-def extract_links_from_pdf_page(page):
+def extract_links_from_pdf(pages):
   links = []
-  text = page.extract_text()
-  lines = text.split("\n")
-  for line in lines:
-    if "http" in line:
-      links.append(line)
-      #TODO: Link may be split over multiple lines, make sure to include the complete link not just the sentence where it started
-  return links
+  doi = ""
+  artifact = []
+  for page in pages:
+    text = page.extract_text()
+    lines = text.split("\n")
+    for line in lines:
+      # Add line if it contains a link, but not if it is a link in the copyright statement (that refers to the paper itself)
+      if "http" in line:
+        # If it's a copyright statement then extract the DOI
+        if "artifact" in line.lower() or "code" in line.lower() or "supplement" in line.lower():
+          artifact.append(line)
+        elif "The Author(s) 2022" in line or "not under copyright protection" in line:
+          if doi == "":
+            doi = "http" + "".join(line.split("http")[1:])
+        # If we didn't find a copyright statement, assume first link is the paper DOI
+        elif doi == "" and "doi" in line:
+            line = line.replace(" ", "")
+            m = re.search("http.*\_[0-9][0-9]", line)
+            doi = m.group() if m else line
+        # Otherwise add the link the list of links (unless it's a useless CC link)
+        elif not "creativecommons.org" in line:
+          links.append(line)
+        #TODO: Link may be split over multiple lines, make sure to include the complete link not just the sentence where it started
+  return doi, artifact, links
 
 # Extracts keywords from a PDF page by looking for a sentence starting with "Keywords:" (as used in the TACAS template)
-def extract_keywords_from_tacas_page(page):
+def extract_keywords_from_pdf(page):
   keywords = []
   text = page.extract_text()
   if "Keywords:" in text:
     text_after_keywords = text.split("Keywords:")[1]
-    words = text_after_keywords.split("1")[0].split("·") # Take all text until section 1 starts then split on the separator
+    words = text_after_keywords.split("1")[0].split("·") # Take all text until section 1 starts, then split on the separator
     keywords.extend(words)
     keywords = [w.strip() for w in keywords]
   return keywords
 
-# Extracts keywords and links from a TACAS paper
-def extract_from_tacas_paper(pages):
+# Extracts keywords and links from a paper (PDF format)
+def extract_keywords_and_links_from_pdf(pages):
   keywords = []
   links = []
-  keywords = extract_keywords_from_tacas_page(pages[0])
-  for page in pages:
-    links += extract_links_from_pdf_page(page)
-  return keywords, links
+  keywords = extract_keywords_from_pdf(pages[0])
+  doi, artifact, links = extract_links_from_pdf(pages)
+  return keywords, doi, artifact, links
 
 
 # Detects boundaries (i.e. first and last page) of all papers in a proceedings pdf
@@ -46,8 +119,7 @@ def extract_from_tacas_paper(pages):
 # Endings are detected by looking for the "Open Access" statement which is placed at the end of each paper
 # Returns 
 def detect_paper_boundaries(reader, paper_titles):
-  beginnings = []
-  ends = []
+  beginnings, bibs, ends = [], [], []
   for i in range(0, len(reader.pages)):
     text = reader.pages[i].extract_text()
     # seems to work fine without 2022 except that it identifies a few more "beginnings" because of long lists of references
@@ -55,25 +127,31 @@ def detect_paper_boundaries(reader, paper_titles):
       beginnings.append(i)
     if "Open Access" in text: # Identify end of papers
       ends.append(i)
+    if "References\n" in text: # Identify beginning of bibliography
+      bibs.append(i)
   # Disregard first element in ends, which refers to the "Open Access" statement in the beginning of the proceedings that is not associated with any specific paper
-  return zip(beginnings, ends[1:])
+  return list(zip(beginnings, bibs, ends[1:]))
   
 
 
 for doc in os.listdir("Proceedings/"):
   reader = PdfReader("Proceedings/"+doc)
   # Identifies paper titles in outline of a PDF document
-  # Note: this works for TACAS papers, did not try with other papers yet
   paper_titles = []
   for dest in reader.outline:
-    for key in dest:
-      if not isinstance(key, list):
-        paper_titles.append(key['/Title'])
-  
+    if "TACAS" in doc:
+      for key in dest:
+        if not isinstance(key, list):
+          paper_titles.append(key['/Title'])
+    elif "CAV" in doc:
+      if not isinstance(dest, list) and dest['/Count'] != 0:
+        paper_titles.append(dest['/Title'])
+  # Detect boundaries (first and last page) of papers, used for grouping other metadata
   paper_pages = detect_paper_boundaries(reader, paper_titles)
-  
-  for begin,end in paper_pages:
-    keywords, links = extract_from_tacas_paper(reader.pages[begin:end+1])
-    print(keywords)
-    print(links)
-    print("\n")
+  i = 0
+  while (i < len(paper_titles)):
+    begin, bib, end = paper_pages[i]
+    keywords, doi, artifact, links = extract_keywords_and_links_from_pdf(reader.pages[begin:end+1])
+    conf = "TACAS" if "TACAS" in doc else "CAV"
+    dump_to_markdown(paper_titles[i], doi, conf, "2022", keywords)
+    i += 1
